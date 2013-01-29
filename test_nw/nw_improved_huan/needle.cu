ce59310f8e861d60af37bff617bdf93e4a99ee96
Huan Truong <htruong@tnhh.net>
2013-01-29 15:20:27 -0600

    Beautified the code

--- test_nw/nw_improved_huan/needle.cu
+++ test_nw/nw_improved_huan/needle.cu
@@ -18,224 +18,234 @@
 
 inline void cudaCheckError(int line, cudaError_t ce)
 {
-	if (ce != cudaSuccess){
-		printf("Error: line %d %s\n", line, cudaGetErrorString(ce));
-		exit(1);
-	}
+    if (ce != cudaSuccess) {
+        printf("Error: line %d %s\n", line, cudaGetErrorString(ce));
+        exit(1);
+    }
 }
 
-int validation(int *score_matrix_cpu, int *score_matrix, unsigned int length) 
+int validation(int *score_matrix_cpu, int *score_matrix, unsigned int length)
 {
-	unsigned int i = 0;
-	while (i!=length){
-		if ( score_matrix_cpu[i]==score_matrix[i] ){
-			++i;
-			continue;
-		}
-		else {
-			printf("i = %d\n",i);	
-			return 0;
-		}	
-	}
-	return 1;
+    unsigned int i = 0;
+    while (i!=length) {
+        if ( score_matrix_cpu[i]==score_matrix[i] ) {
+            ++i;
+            continue;
+        }
+        else {
+            printf("i = %d\n",i);
+            return 0;
+        }
+    }
+    return 1;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // declaration, forward
 void runTest( int argc, char** argv);
 
-double gettime(){
-  struct timeval t;
-  gettimeofday(&t,NULL);
-  return t.tv_sec+t.tv_usec*1e-6;
+double gettime() {
+    struct timeval t;
+    gettimeofday(&t,NULL);
+    return t.tv_sec+t.tv_usec*1e-6;
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Program main
 ////////////////////////////////////////////////////////////////////////////////
-int main( int argc, char** argv) 
+int main( int argc, char** argv)
 {
-	runTest( argc, argv);
+    runTest( argc, argv);
     return EXIT_SUCCESS;
 }
 
 void usage(int argc, char **argv)
 {
-	fprintf(stderr, "Usage: %s <pair number> <penalty> \n", argv[0]);
-	fprintf(stderr, "\t<pair number>  - times of comparison\n");
-	fprintf(stderr, "\t<penalty> - penalty(negative integer)\n");
-	exit(1);
+    fprintf(stderr, "Usage: %s <pair number> <penalty> \n", argv[0]);
+    fprintf(stderr, "\t<pair number>  - times of comparison\n");
+    fprintf(stderr, "\t<penalty> - penalty(negative integer)\n");
+    exit(1);
 }
 
-void runTest( int argc, char** argv) 
+void runTest( int argc, char** argv)
 {
-	double time, end_time;
-	int pair_num;
-	short penalty;
+    double time, end_time;
+    int pair_num;
+    short penalty;
     char sequence_set1[MAX_SEQ_LEN*MAX_SEQ_NUM] = {0}, sequence_set2[MAX_SEQ_LEN*MAX_SEQ_NUM] = {0};
-	unsigned int pos1[MAX_SEQ_NUM] = {0}, pos2[MAX_SEQ_NUM] = {0}, pos_matrix[MAX_SEQ_NUM] = {0};
-	int *score_matrix;
-	int *score_matrix_cpu;
-	char *d_sequence_set1, *d_sequence_set2;
-	unsigned int *d_pos1, *d_pos2, *d_pos_matrix;
-	int *d_score_matrix;
-	int seq1_len, seq2_len;
+    unsigned int pos1[MAX_SEQ_NUM] = {0}, pos2[MAX_SEQ_NUM] = {0}, pos_matrix[MAX_SEQ_NUM] = {0};
+    int *score_matrix;
+    int *score_matrix_cpu;
+    char *d_sequence_set1, *d_sequence_set2;
+    unsigned int *d_pos1, *d_pos2, *d_pos_matrix;
+    int *d_score_matrix;
+    int seq1_len, seq2_len;
 
-	if (argc == 3)
-	{
-		pair_num = atoi(argv[1]);
-		penalty = atoi(argv[2]);
-		if (pair_num>MAX_SEQ_NUM){
-			fprintf(stderr, "\t<pair number>  - times of comparison should be less than %d\n",MAX_SEQ_NUM);
-			exit(1);
-		}
-	}
-    else{
-		usage(argc, argv);
+    if (argc == 3)
+    {
+        pair_num = atoi(argv[1]);
+        penalty = atoi(argv[2]);
+        if (pair_num>MAX_SEQ_NUM) {
+            fprintf(stderr, "\t<pair number>  - times of comparison should be less than %d\n",MAX_SEQ_NUM);
+            exit(1);
+        }
+    }
+    else {
+        usage(argc, argv);
     }
 
-	// first API
-	time = gettime();
-	cudaCheckError( __LINE__, cudaSetDevice(0) );
-	
-	end_time = gettime();
-	fprintf(stdout,"First API,%lf\n",end_time-time);
-	time = end_time;
-	
-	// Get input data
-	  
-	srand ( 7 );
-	pos_matrix[0] = pos1[0] = pos2[0] = 0;
-	for (int i=0; i<pair_num; ++i){
-		//please define your own sequence 1
-		seq1_len = 2048; //64+rand() % 20;
-		//printf("Seq1 length: %d\n", seq1_len);	
-		for (int j=0; j<seq1_len; ++j)		
-			sequence_set1[ pos1[i] + j ] = rand() % 20 + 1;
-		pos1[i+1] = pos1[i] + seq1_len;
-		//please define your own sequence 2.
-		seq2_len = 2048;//64+rand() % 20;		
-		//printf("Seq2 length: %d\n\n", seq2_len);		
-		for (int j=0; j<seq2_len; ++j)		
-			sequence_set2[ pos2[i] +j ] = rand() % 20 + 1;
-		pos2[i+1] = pos2[i] + seq2_len;
-		//printf("Matrix size increase: %d\n", (seq1_len+1) * (seq2_len+1));
-		pos_matrix[i+1] = pos_matrix[i] + (seq1_len+1) * (seq2_len+1);
-	}
-	score_matrix = (int *)malloc( pos_matrix[pair_num]*sizeof(int));
-	score_matrix_cpu = (int *)malloc( pos_matrix[pair_num]*sizeof(int)); 
-  printf ("Allocating %dMB of memory... (sizeof int=%d bytes)", pos_matrix[pair_num]*sizeof(int)/1024/1024, sizeof(int));
-	needleman_cpu(sequence_set1, sequence_set2, pos1, pos2, score_matrix_cpu, pos_matrix, pair_num, penalty);
+    // first API
+    time = gettime();
+    cudaCheckError( __LINE__, cudaSetDevice(0) );
 
-	// printf("Start Needleman-Wunsch\n");
+    end_time = gettime();
+    fprintf(stdout,"First API,%lf\n",end_time-time);
+    time = end_time;
 
-	cudaCheckError( __LINE__, cudaMalloc( (void**)&d_sequence_set1, sizeof(char)*pos1[pair_num] ) );
-	cudaCheckError( __LINE__, cudaMalloc( (void**)&d_sequence_set2, sizeof(char)*pos2[pair_num] ) );
-	cudaCheckError( __LINE__, cudaMalloc( (void**)&d_score_matrix, sizeof(int)*pos_matrix[pair_num]) );
-	cudaCheckError( __LINE__, cudaMalloc( (void**)&d_pos1, sizeof(unsigned int)*(pair_num+1) ) );
-	cudaCheckError( __LINE__, cudaMalloc( (void**)&d_pos2, sizeof(unsigned int)*(pair_num+1) ) );
- 	cudaCheckError( __LINE__, cudaMalloc( (void**)&d_pos_matrix, sizeof(unsigned int)*(pair_num+1) ) );
+    // Get input data
 
-	// CPU phases
-	end_time = gettime();
-	fprintf(stdout,"CPU,%lf\n",end_time-time);
-	time = end_time;
-	
-	// Memcpy to device
-	cudaCheckError( __LINE__, cudaMemcpy( d_sequence_set1, sequence_set1, sizeof(char)*pos1[pair_num], cudaMemcpyHostToDevice ) );
-	cudaCheckError( __LINE__, cudaMemcpy( d_sequence_set2, sequence_set2, sizeof(char)*pos2[pair_num], cudaMemcpyHostToDevice ) );
-	cudaCheckError( __LINE__, cudaMemcpy( d_pos1, pos1, sizeof(unsigned int)*(pair_num+1), cudaMemcpyHostToDevice ) );
-	cudaCheckError( __LINE__, cudaMemcpy( d_pos2, pos2, sizeof(unsigned int)*(pair_num+1), cudaMemcpyHostToDevice ) );
- 	cudaCheckError( __LINE__, cudaMemcpy( d_pos_matrix, pos_matrix, sizeof(unsigned int)*(pair_num+1), cudaMemcpyHostToDevice ) );
-	
-	//end_time = gettime();
-	//fprintf(stdout,"Memcpy to device,%lf\n",end_time-time);
-	//time = end_time;
+    srand ( 7 );
+    pos_matrix[0] = pos1[0] = pos2[0] = 0;
+    for (int i=0; i<pair_num; ++i) {
+        //please define your own sequence 1
+        seq1_len = 2048; //64+rand() % 20;
+        //printf("Seq1 length: %d\n", seq1_len);
+        for (int j=0; j<seq1_len; ++j)
+            sequence_set1[ pos1[i] + j ] = rand() % 20 + 1;
+        pos1[i+1] = pos1[i] + seq1_len;
+        //please define your own sequence 2.
+        seq2_len = 2048;//64+rand() % 20;
+        //printf("Seq2 length: %d\n\n", seq2_len);
+        for (int j=0; j<seq2_len; ++j)
+            sequence_set2[ pos2[i] +j ] = rand() % 20 + 1;
+        pos2[i+1] = pos2[i] + seq2_len;
+        //printf("Matrix size increase: %d\n", (seq1_len+1) * (seq2_len+1));
+        pos_matrix[i+1] = pos_matrix[i] + (seq1_len+1) * (seq2_len+1);
+    }
+    score_matrix = (int *)malloc( pos_matrix[pair_num]*sizeof(int));
+    score_matrix_cpu = (int *)malloc( pos_matrix[pair_num]*sizeof(int));
+    printf ("Allocating %dMB of memory... (sizeof int=%d bytes)", pos_matrix[pair_num]*sizeof(int)/1024/1024, sizeof(int));
+    needleman_cpu(sequence_set1, sequence_set2, pos1, pos2, score_matrix_cpu, pos_matrix, pair_num, penalty);
 
-	// the threads in block should equal to the STRIDE_SIZE
-/*	needleman_cuda_dynamic<<<14, 128>>>(d_sequence_set1, d_sequence_set2, 
-									   d_pos1, d_pos2,
-									   d_score_matrix, d_pos_matrix,
-									   pair_num, penalty);
-*/
-	needleman_cuda_diagonal<<<pair_num,512>>>(d_sequence_set1, d_sequence_set2, 
-									   d_pos1, d_pos2,
-									   d_score_matrix, d_pos_matrix,
-									   pair_num, penalty);
-	cudaCheckError( __LINE__, cudaDeviceSynchronize() );
-	//end_time = gettime();
-	//fprintf(stdout,"kernel,%lf\n",end_time-time);
-	//time = end_time;
-	// Memcpy to host
-	cudaCheckError( __LINE__, cudaMemcpy( score_matrix, d_score_matrix, sizeof(int)*pos_matrix[pair_num], cudaMemcpyDeviceToHost ) );    
+    // printf("Start Needleman-Wunsch\n");
 
-	end_time = gettime();
-	//fprintf(stdout,"Memcpy to host,%lf\n",end_time-time);
-	fprintf(stdout,"Total CUDA implementation time, %lf\n",end_time-time);
-	time = end_time;
-	
-	if ( validation(score_matrix_cpu, score_matrix, pos_matrix[pair_num]) )		
-		printf("Validation: PASS\n");
-	else
-		printf("Validation: FAIL\n");		
+    cudaCheckError( __LINE__, cudaMalloc( (void**)&d_sequence_set1, sizeof(char)*pos1[pair_num] ) );
+    cudaCheckError( __LINE__, cudaMalloc( (void**)&d_sequence_set2, sizeof(char)*pos2[pair_num] ) );
+    cudaCheckError( __LINE__, cudaMalloc( (void**)&d_score_matrix, sizeof(int)*pos_matrix[pair_num]) );
+    cudaCheckError( __LINE__, cudaMalloc( (void**)&d_pos1, sizeof(unsigned int)*(pair_num+1) ) );
+    cudaCheckError( __LINE__, cudaMalloc( (void**)&d_pos2, sizeof(unsigned int)*(pair_num+1) ) );
+    cudaCheckError( __LINE__, cudaMalloc( (void**)&d_pos_matrix, sizeof(unsigned int)*(pair_num+1) ) );
+
+    // CPU phases
+    end_time = gettime();
+    fprintf(stdout,"CPU,%lf\n",end_time-time);
+    time = end_time;
+
+    // Memcpy to device
+    cudaCheckError( __LINE__, cudaMemcpy( d_sequence_set1, sequence_set1, sizeof(char)*pos1[pair_num], cudaMemcpyHostToDevice ) );
+    cudaCheckError( __LINE__, cudaMemcpy( d_sequence_set2, sequence_set2, sizeof(char)*pos2[pair_num], cudaMemcpyHostToDevice ) );
+    cudaCheckError( __LINE__, cudaMemcpy( d_pos1, pos1, sizeof(unsigned int)*(pair_num+1), cudaMemcpyHostToDevice ) );
+    cudaCheckError( __LINE__, cudaMemcpy( d_pos2, pos2, sizeof(unsigned int)*(pair_num+1), cudaMemcpyHostToDevice ) );
+    cudaCheckError( __LINE__, cudaMemcpy( d_pos_matrix, pos_matrix, sizeof(unsigned int)*(pair_num+1), cudaMemcpyHostToDevice ) );
+
+    //end_time = gettime();
+    //fprintf(stdout,"Memcpy to device,%lf\n",end_time-time);
+    //time = end_time;
+
+    // the threads in block should equal to the STRIDE_SIZE
+    /*	needleman_cuda_dynamic<<<14, 128>>>(d_sequence_set1, d_sequence_set2,
+    									   d_pos1, d_pos2,
+    									   d_score_matrix, d_pos_matrix,
+    									   pair_num, penalty);
+    */
+    needleman_cuda_diagonal<<<pair_num,512>>>(d_sequence_set1, d_sequence_set2,
+            d_pos1, d_pos2,
+            d_score_matrix, d_pos_matrix,
+            pair_num, penalty);
+    cudaCheckError( __LINE__, cudaDeviceSynchronize() );
+    //end_time = gettime();
+    //fprintf(stdout,"kernel,%lf\n",end_time-time);
+    //time = end_time;
+    // Memcpy to host
+    cudaCheckError( __LINE__, cudaMemcpy( score_matrix, d_score_matrix, sizeof(int)*pos_matrix[pair_num], cudaMemcpyDeviceToHost ) );
+
+    end_time = gettime();
+    //fprintf(stdout,"Memcpy to host,%lf\n",end_time-time);
+    fprintf(stdout,"Total CUDA implementation time, %lf\n",end_time-time);
+    time = end_time;
+
+    if ( validation(score_matrix_cpu, score_matrix, pos_matrix[pair_num]) )
+        printf("Validation: PASS\n");
+    else
+        printf("Validation: FAIL\n");
 
 #ifdef TRACEBACK
-	for (int i = max_rows - 2,  j = max_rows - 2; i>=0, j>=0;){
-		int nw, n, w, traceback;
-		if ( i == max_rows - 2 && j == max_rows - 2 )
-			//fprintf(fpo, "%d ", output_itemsets[ i * max_cols + j]); //print the first element
-		if ( i == 0 && j == 0 )
-           break;
-		if ( i > 0 && j > 0 ){
-			nw = output_itemsets[(i - 1) * max_cols + j - 1];
-		    w  = output_itemsets[ i * max_cols + j - 1 ];
+    for (int i = max_rows - 2,  j = max_rows - 2; i>=0, j>=0;) {
+        int nw, n, w, traceback;
+        if ( i == max_rows - 2 && j == max_rows - 2 )
+            //fprintf(fpo, "%d ", output_itemsets[ i * max_cols + j]); //print the first element
+            if ( i == 0 && j == 0 )
+                break;
+        if ( i > 0 && j > 0 ) {
+            nw = output_itemsets[(i - 1) * max_cols + j - 1];
+            w  = output_itemsets[ i * max_cols + j - 1 ];
             n  = output_itemsets[(i - 1) * max_cols + j];
-		}
-		else if ( i == 0 ){
-		    nw = n = LIMIT;
-		    w  = output_itemsets[ i * max_cols + j - 1 ];
-		}
-		else if ( j == 0 ){
-		    nw = w = LIMIT;
+        }
+        else if ( i == 0 ) {
+            nw = n = LIMIT;
+            w  = output_itemsets[ i * max_cols + j - 1 ];
+        }
+        else if ( j == 0 ) {
+            nw = w = LIMIT;
             n  = output_itemsets[(i - 1) * max_cols + j];
-		}
-		else{
-		}
+        }
+        else {
+        }
 
-		//traceback = maximum(nw, w, n);
-		int new_nw, new_w, new_n;
-		new_nw = nw + referrence[i * max_cols + j];
-		new_w = w - penalty;
-		new_n = n - penalty;
-		
-		traceback = maximum(new_nw, new_w, new_n);
-		if(traceback == new_nw)
-			traceback = nw;
-		if(traceback == new_w)
-			traceback = w;
-		if(traceback == new_n)
+        //traceback = maximum(nw, w, n);
+        int new_nw, new_w, new_n;
+        new_nw = nw + referrence[i * max_cols + j];
+        new_w = w - penalty;
+        new_n = n - penalty;
+
+        traceback = maximum(new_nw, new_w, new_n);
+        if(traceback == new_nw)
+            traceback = nw;
+        if(traceback == new_w)
+            traceback = w;
+        if(traceback == new_n)
             traceback = n;
-			
-		//fprintf(fpo, "%d ", traceback);
 
-		if(traceback == nw )
-		{i--; j--; continue;}
+        //fprintf(fpo, "%d ", traceback);
+
+        if(traceback == nw )
+        {
+            i--;
+            j--;
+            continue;
+        }
 
         else if(traceback == w )
-		{j--; continue;}
+        {
+            j--;
+            continue;
+        }
 
         else if(traceback == n )
-		{i--; continue;};
-	}
+        {
+            i--;
+            continue;
+        };
+    }
 #endif
 //	fclose(fpo);
-	cudaFree(d_sequence_set1);
-	cudaFree(d_sequence_set2);
-	cudaFree(d_pos1);
-	cudaFree(d_pos2);
- 	cudaFree(d_pos_matrix);
-	cudaFree(d_score_matrix);
-	free(score_matrix);
-	
+    cudaFree(d_sequence_set1);
+    cudaFree(d_sequence_set2);
+    cudaFree(d_pos1);
+    cudaFree(d_pos2);
+    cudaFree(d_pos_matrix);
+    cudaFree(d_score_matrix);
+    free(score_matrix);
+
 }
--- test_nw/nw_improved_huan/needle_kernel_dynamic.cu
+++ test_nw/nw_improved_huan/needle_kernel_dynamic.cu
@@ -5,89 +5,99 @@
 #define STRIDE_SIZE 128		// equal to the number of thread per block
 #define PAIR_IN_BLOCK 32
 
-__global__ void needleman_cuda_dynamic(char *sequence_set1, char *sequence_set2, 
-									   unsigned int *pos1, unsigned int *pos2,
-									   short *score_matrix, unsigned int *pos_matrix,
-									   unsigned int max_pair_no, short penalty)
-{ 
-	__shared__ char s_seq1[PAIR_IN_BLOCK][STRIDE_SIZE+1];
-	__shared__ short s_line1[PAIR_IN_BLOCK][STRIDE_SIZE+1];
-	__shared__ short s_line2[PAIR_IN_BLOCK][STRIDE_SIZE+1];
-	short *pl1, *pl2, *ptmp, *matrix;
-	char seq2_ch, *seq1, *seq2;
-	unsigned int start_pair_no = blockIdx.x * PAIR_IN_BLOCK;
-	unsigned int end_pair_no = (blockIdx.x+1)*PAIR_IN_BLOCK<max_pair_no? (blockIdx.x+1)*PAIR_IN_BLOCK:max_pair_no; 
-	unsigned int tid = threadIdx.x;
-	unsigned int seq1_len = pos1[start_pair_no+1] - pos1[start_pair_no];
-	unsigned int seq2_len;
-	unsigned int stride = seq1_len / STRIDE_SIZE;
-	int i, j, k, index_x, index_y;
-	if ( (seq1_len % STRIDE_SIZE)!=0 ) stride++;
-	
-	if ( start_pair_no<end_pair_no ){
-	// Divide the long array into different stride
-	for (i=0; i<stride; ++i) {
-		/************** Initialization **************/
-		index_x = 0; index_y = tid + i*STRIDE_SIZE+1;
-		/* Here, we should add a bunch of assignment statement */		
-		for (j=start_pair_no; j<end_pair_no; ++j) {	// iteration between different loops
-			if ( i==0 )	s_line1[j-start_pair_no][0] = *(score_matrix + pos_matrix[j]) = 0;
-			else	s_line1[j-start_pair_no][0] = matrix[i*STRIDE_SIZE];
-			unsigned int load_stride = STRIDE_SIZE/blockDim.x;
-			for ( k=0; k<load_stride; ++k ) {
-				index_y = tid + i*STRIDE_SIZE+1 + k*blockDim.x;			
-				if ( index_y<=seq1_len ) {			
-					matrix = score_matrix + pos_matrix[j];
-					matrix[index_y] = s_line1[j-start_pair_no][tid+1+k*blockDim.x] = penalty * index_y;
-					seq1 = sequence_set1 + pos1[j];
-					s_seq1[j-start_pair_no][tid+1+k*blockDim.x] = seq1[index_y-1];
-				}
-			}
-		}
-		/**************************************/
-		
-		/************* fill the matrix **************/
-		pl1 = s_line1[tid];
-		pl2 = s_line2[tid];
-		seq2_len = pos2[start_pair_no+1] - pos2[start_pair_no];
-		for (index_x=1; index_x<=seq2_len; ++index_x){	// each iteration stands for each row
-			if ( start_pair_no+tid<end_pair_no ) {		// seq2_len should be the maximum length
-				seq2 = sequence_set2 + pos2[start_pair_no+tid];
-				seq1_len = pos1[start_pair_no+tid+1] - pos1[start_pair_no+tid];	// length
-				matrix = score_matrix + pos_matrix[start_pair_no+tid];
-				index_y = i*STRIDE_SIZE+1;
-				/************* Calculation **************/
-				seq2_ch = seq2[index_x-1];
-				if (i==0)  pl2[0] = matrix[index_x*(seq1_len+1)] = penalty * index_x;	// initialize the first column
-				else pl2[0] = matrix[index_x*(seq1_len+1) + i*STRIDE_SIZE];			// load the data from last iteration
-			
-				for (k=0; k<STRIDE_SIZE; ++k) {
-					pl2[1+k] = maximum(pl2[k]+penalty,		\
-									   pl1[1+k]+penalty,	\
-									   pl1[k]+blosum62[seq2_ch][s_seq1[tid][k+1]]);
-				}
-				/************* swap the pointer ****************/
-				ptmp = pl1;	pl1 = pl2; pl2 = ptmp;
-			}
-			__syncthreads();
-			/*********** store data to global memory **********/	
-			index_y = tid + i*STRIDE_SIZE+1;		
-			for (j=start_pair_no; j<end_pair_no; ++j) {
-				unsigned int load_stride = STRIDE_SIZE/blockDim.x;
-				for ( k=0; k<load_stride; ++k ) {
-					index_y = tid + i*STRIDE_SIZE+1 + k*blockDim.x;	
-					if ( index_y<=seq1_len ) {		
-						matrix = score_matrix + pos_matrix[j];				
-						seq1_len = pos1[j+1] - pos1[j];		
-						if ( index_x&1 == 1)
-							matrix[index_x*(seq1_len+1)+index_y] = s_line2[j-start_pair_no][tid+1+k*blockDim.x];
-						else
-							matrix[index_x*(seq1_len+1)+index_y] = s_line1[j-start_pair_no][tid+1+k*blockDim.x];
+__global__ void needleman_cuda_dynamic(char *sequence_set1, char *sequence_set2,
+                                       unsigned int *pos1, unsigned int *pos2,
+                                       short *score_matrix, unsigned int *pos_matrix,
+                                       unsigned int max_pair_no, short penalty)
+{
+    __shared__ char s_seq1[PAIR_IN_BLOCK][STRIDE_SIZE+1];
+    __shared__ short s_line1[PAIR_IN_BLOCK][STRIDE_SIZE+1];
+    __shared__ short s_line2[PAIR_IN_BLOCK][STRIDE_SIZE+1];
+    short *pl1, *pl2, *ptmp, *matrix;
+    char seq2_ch, *seq1, *seq2;
+    unsigned int start_pair_no = blockIdx.x * PAIR_IN_BLOCK;
+    unsigned int end_pair_no = (blockIdx.x+1)*PAIR_IN_BLOCK<max_pair_no? (blockIdx.x+1)*PAIR_IN_BLOCK:max_pair_no;
+    unsigned int tid = threadIdx.x;
+    unsigned int seq1_len = pos1[start_pair_no+1] - pos1[start_pair_no];
+    unsigned int seq2_len;
+    unsigned int stride = seq1_len / STRIDE_SIZE;
+    int i, j, k, index_x, index_y;
+    if ( (seq1_len % STRIDE_SIZE)!=0 ) stride++;
+
+    if ( start_pair_no<end_pair_no ) {
+        // Divide the long array into different stride
+        for (i=0; i<stride; ++i) {
+            /************** Initialization **************/
+            index_x = 0;
+            index_y = tid + i*STRIDE_SIZE+1;
+            /* Here, we should add a bunch of assignment statement */
+            for (j=start_pair_no; j<end_pair_no; ++j) {	// iteration between different loops
+                if ( i==0 )	s_line1[j-start_pair_no][0] = *(score_matrix + pos_matrix[j]) = 0;
+                else	s_line1[j-start_pair_no][0] = matrix[i*STRIDE_SIZE];
+                unsigned int load_stride = STRIDE_SIZE/blockDim.x;
+                for ( k=0; k<load_stride; ++k ) {
+                    index_y = tid + i*STRIDE_SIZE+1 + k*blockDim.x;
+                    if ( index_y<=seq1_len ) {
+                        matrix = score_matrix + pos_matrix[j];
+                        matrix[index_y] = s_line1[j-start_pair_no][tid+1+k*blockDim.x] = penalty * index_y;
+                        seq1 = sequence_set1 + pos1[j];
+                        s_seq1[j-start_pair_no][tid+1+k*blockDim.x] = seq1[index_y-1];
+                    }
+                }
+            }
+            /**************************************/
+
+            /************* fill the matrix **************/
+            pl1 = s_line1[tid];
+            pl2 = s_line2[tid];
+            seq2_len = pos2[start_pair_no+1] - pos2[start_pair_no];
+            for (index_x=1; index_x<=seq2_len; ++index_x) {	// each iteration stands for each row
+                if ( start_pair_no+tid<end_pair_no ) {		// seq2_len should be the maximum length
+                    seq2 = sequence_set2 + pos2[start_pair_no+tid];
+                    seq1_len = pos1[start_pair_no+tid+1] - pos1[start_pair_no+tid];	// length
+                    matrix = score_matrix + pos_matrix[start_pair_no+tid];
+                    index_y = i*STRIDE_SIZE+1;
+                    /************* Calculation **************/
+                    seq2_ch = seq2[index_x-1];
+                    if (i==0)  {
+						// initialize the first column
+						pl2[0] = matrix[index_x*(seq1_len+1)] = penalty * index_x;
+					} else {
+						// load the data from last iteration
+						pl2[0] = matrix[index_x*(seq1_len+1) + i*STRIDE_SIZE];
 					}
-				}
-			}
-			__syncthreads();
-		}
-	}	//end of for
-	}	//end of if
+
+					// Fix here for traceback
+                    for (k=0; k<STRIDE_SIZE; ++k) {
+                        pl2[1+k] = maximum(
+							pl2[k]+penalty,		
+                            pl1[1+k]+penalty,
+                            pl1[k]+blosum62[seq2_ch][s_seq1[tid][k+1]]);
+                    }
+                    /************* swap the pointer ****************/
+                    ptmp = pl1;
+                    pl1 = pl2;
+                    pl2 = ptmp;
+                }
+                __syncthreads();
+                /*********** store data to global memory **********/
+                index_y = tid + i*STRIDE_SIZE+1;
+                for (j=start_pair_no; j<end_pair_no; ++j) {
+                    unsigned int load_stride = STRIDE_SIZE/blockDim.x;
+                    for ( k=0; k<load_stride; ++k ) {
+                        index_y = tid + i*STRIDE_SIZE+1 + k*blockDim.x;
+                        if ( index_y<=seq1_len ) {
+                            matrix = score_matrix + pos_matrix[j];
+                            seq1_len = pos1[j+1] - pos1[j];
+                            if ( index_x&1 == 1)
+                                matrix[index_x*(seq1_len+1)+index_y] = s_line2[j-start_pair_no][tid+1+k*blockDim.x];
+                            else
+                                matrix[index_x*(seq1_len+1)+index_y] = s_line1[j-start_pair_no][tid+1+k*blockDim.x];
+                        }
+                    }
+                }
+                __syncthreads();
+            }
+        }	//end of for
+    }	//end of if
 }
