96bc97eb87b4443737121bf72100fa7816b25fe7
Huan Truong <htruong@tnhh.net>
2013-02-16 21:00:58 -0600

    new shit

--- test_nw/scorematrix_dbuffer/bench.sh
+++ test_nw/scorematrix_dbuffer/bench.sh
@@ -2,8 +2,8 @@ for i in 8 16 24 32 48 64 128
 do
 	for j in 1 2 3
 	do
-		./apitest_regular $i >> results_regular.txt
-		./apitest_pinned $i >> results_pinned.txt
-		./apitest_dualbuffer_pinned $i >> results_df_pinned.txt
+		./apitest_regular $i -10 >> results_regular.txt
+		./apitest_pinned $i -10 >> results_pinned.txt
+		./apitest_dualbuffer_pinned $i -10 >> results_df_pinned.txt
 	done
 done
