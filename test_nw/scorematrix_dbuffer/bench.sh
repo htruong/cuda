5b8cdb44b3bdff1eabac844fd75b1ee9681071d2
Huan Truong <htruong@tnhh.net>
2013-02-16 21:48:05 -0600

    New bench shit

--- test_nw/scorematrix_dbuffer/bench.sh
+++ test_nw/scorematrix_dbuffer/bench.sh
@@ -10,3 +10,8 @@ do
 		./apitest_dualbuffer_pinned $i -10 | tee -a results_df_pinned.txt
 	done
 done
+cat  results_regular.txt  | egrep "TESTING| calc" | sed -e 's/[^-]* \([0-9\\.]\+\).~*/\1/' | sed -e 's/-//g' | sed -e 's/\([0-9]\+\)PAIRS/NL\1/' | tr "\\n" "," | tr "NL" "\\n" > results_regular.csv
+cat  results_pinned.txt  | egrep "TESTING| calc" | sed -e 's/[^-]* \([0-9\\.]\+\).~*/\1/' | sed -e 's/-//g' | sed -e 's/\([0-9]\+\)PAIRS/NL\1/' | tr "\\n" "," | tr "NL" "\\n" > results_pinned.csv
+cat  results_dualbuffer_pinned.txt  | egrep "TESTING| calc" | sed -e 's/[^-]* \([0-9\\.]\+\).~*/\1/' | sed -e 's/-//g' | sed -e 's/\([0-9]\+\)PAIRS/NL\1/' | tr "\\n" "," | tr "NL" "\\n" > results_dualbuffer_pinned.csv
+
+
