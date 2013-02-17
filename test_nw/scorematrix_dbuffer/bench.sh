e1af5aadd98e62191a2821fd0d46fb1b5edf583c
Huan Truong <htruong@tnhh.net>
2013-02-17 13:19:28 -0600

    New bench

--- test_nw/scorematrix_dbuffer/bench.sh
+++ test_nw/scorematrix_dbuffer/bench.sh
@@ -3,7 +3,10 @@ i=3
 	do
 		./apitest_regular $i -10 | tee -a results_regular.txt
 		./apitest_pinned $i -10 | tee -a results_pinned.txt
-		./apitest_dualbuffer_pinned $i -10 | tee -a results_df_pinned.txt
+		./apitest_dualbuffer_pinned $i -10 | tee -a results_db_pinned.txt
 	done
+cat results_db_pinned.txt | grep "CSV_ALL" > results_df_pinned.csv
+cat results_pinned.txt | grep "CSV_ALL" > results_pinned.csv
+cat results_regular.txt | grep "CSV_ALL" > results_regular.csv
 
 
