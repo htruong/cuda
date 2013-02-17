a139835d8d26626717c35a6ec405794ed1fcb162
Huan Truong <htruong@tnhh.net>
2013-02-16 21:22:05 -0600

    New shit

--- test_nw/scorematrix_dbuffer/bench.sh
+++ test_nw/scorematrix_dbuffer/bench.sh
@@ -1,12 +1,12 @@
 for i in 8 16 24 32 48 64 128
 do
- 	echo -e "----- TESTING WITH $i PAIRS ----" >> results_regular.txt
- 	echo -e "----- TESTING WITH $i PAIRS ----" >> results_pinned.txt
- 	echo -e "----- TESTING WITH $i PAIRS ----" >> results_df_pinned.txt
+ 	echo -e "----- TESTING WITH $i PAIRS ----" | tee -a results_regular.txt
+ 	echo -e "----- TESTING WITH $i PAIRS ----" | tee -a results_pinned.txt
+ 	echo -e "----- TESTING WITH $i PAIRS ----" | tee -a results_df_pinned.txt
 	for j in 1 2 3
 	do
-		./apitest_regular $i -10 | tee results_regular.txt
-		./apitest_pinned $i -10 | tee results_pinned.txt
-		./apitest_dualbuffer_pinned $i -10 | tee results_df_pinned.txt
+		./apitest_regular $i -10 | tee -a results_regular.txt
+		./apitest_pinned $i -10 | tee -a results_pinned.txt
+		./apitest_dualbuffer_pinned $i -10 | tee -a results_df_pinned.txt
 	done
 done
